24ac5a81cd6542429700030fcc232f183002c6bf
Dan Cross <dancross96@gmail.com>
2022-08-24 12:42:09 -0500

    added show action for shows on backend

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,4 +3,9 @@ class ShowsController < ApplicationController
     shows = Show.all
     render json: shows.as_json
   end
+  
+  def show
+    show = Show.find_by(id: params[:id])
+    render json: show.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/shows" => "shows#index"
+  get "/shows/:id" => "shows#show"
 end
